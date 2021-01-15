f45340bb403496f86b58749e1cd166c735889c43
hong <417670161@qq.com>
2021-01-15 22:14:40 +0800

    Initial commit

---
+++ editor/cn/1_two-sum.cpp
@@ -0,0 +1,63 @@
+//Given an array of integers nums and an integer target, return indices of the t
+//wo numbers such that they add up to target. 
+//
+// You may assume that each input would have exactly one solution, and you may n
+//ot use the same element twice. 
+//
+// You can return the answer in any order. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 2 <= nums.length <= 103 
+// -109 <= nums[i] <= 109 
+// -109 <= target <= 109 
+// Only one valid answer exists. 
+// 
+// Related Topics 数组 哈希表 
+// 👍 10054 👎 0
+
+#include "include/headers.h"
+
+using namespace std;
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+int main()
+{
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+    //vector<int> ans = s.twoSum(data,11);
+    //cout << ans[0]<<ans[1]<<endl;
+    cout<<"Hello LeetCode"<<endl;
+}
